fc43dc58800306910718036293d56217109c0ab2
Stephen Bourke <steveberky@hotmail.com>
2019-02-25 17:04:32 +0000

    Add project files.

---
+++ EntAppSecond/Pages/Model/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Validation.Pages.Model
+{
+    public class Student
+    {
+
+        [Required]
+        [RegularExpression(@"^(s|S)\d{7}$", ErrorMessage ="ID Must be of format s1234567")]
+        public string StudentID { get; set; } = "";
+
+
+        [Required]
+        [Display(Name = "First Name")]
+        [RegularExpression(@"\S{2,}", ErrorMessage ="First Name must be at least two characters")]
+        public string FirstName { get; set; } = "";
+
+
+        [Required]
+        [RegularExpression(@"\S{3,}", ErrorMessage = "Last Name must be at least three characters")]
+        public string LastName { get; set; } = "";
+
+
+
+        public DateTime DOB { get; set; }
+
+
+        public int NrModules { get; set; }
+
+
+        public int Height { get; set; }
+
+
+        public Student()
+        {
+
+        }
+
+    }
+}
---
+++ EntAppSecond/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Validation.Pages.Model;
+
+namespace Validation.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        public Student Student { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
